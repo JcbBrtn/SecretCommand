979b7d216b776d90a9a1c226bb41eb73ace55150
jcbbrtn <jcbbrtn2@gmail.com>
2022-02-04 08:03:31 -0500

    Created Lobbies and command selector

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 from flask import render_template, Flask, request, redirect, session
 from flask_session import Session
+import random
 
 app = Flask(__name__)
-app = Flask(__name__)
-app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["USER_PERMANENT_SESSION"] = False
 Session(app)
 
 lobbies = {} #A dictionary to hold our lobby sessions... what's a database???
@@ -34,34 +34,51 @@ def index():
         else:
             return redirect('/lobby/' + str(get_new_lobby_num()))
 
+@app.route('/disconnect/<lobbyNum>')
+def disconnect(lobbyNum):
+    global lobbies
+    del lobbies[lobbyNum]
+
 @app.route('/lobby/<lobbyNum>', methods=['GET', 'POST'])
 def lobby(lobbyNum):
     global lobbies
     if lobbyNum not in lobbies.keys():
         #Check to see if this is a new lobby
         lobbies[lobbyNum] = lob()
+        print('Created a new session')
 
     try:
-        a = session['id']
-        print(a)
+        id = session['id']
+        print(f'Session {id} is in lobby #{lobbyNum}')
     except:
         session['id'] = lobbies[lobbyNum].give_num()
+        id = session['id']
+        print('Gave this session a new lobby num')
+
+    if id not in lobbies[lobbyNum].lobby:
+        lobbies[lobbyNum].lobby.append(session['id'])
 
     if lobbies[lobbyNum].isReady:
-        return redirect('/game/<lobbyNum>')
+        return redirect('/game/' + str(lobbyNum))
 
     isReady = bool(request.args.get('ready'))
     if isReady:
         #This would be the first instance of a player being ready. So assign the commander here
         lobbies[lobbyNum].isReady = True
-        return redirect('/game/<lobbyNum>')
+        commanderID = random.choice(lobbies[lobbyNum].lobby)
+        lobbies[lobbyNum].commandID = commanderID
+        return redirect('/game/' + str(lobbyNum))
 
     else:
-        return render_template('lobby.html', lobbyNum=lobbyNum)
+        personCount=len(lobbies[lobbyNum].lobby)
+        return render_template('lobby.html', lobbyNum=lobbyNum, personCount=personCount)
 
 @app.route('/game/<lobbyNum>', methods=['GET', 'POST'])
 def game(lobbyNum):
-    return 'Hi!'
+    if lobbies[lobbyNum].commandID == session['id']:
+        return '<h1>You get a command</h1>'
+    else:
+        return '<h1>no command for you</h1>'
 
 if __name__ == "__main__":
     app.run()
